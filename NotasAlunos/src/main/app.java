4b281223eb62e5e41d0af7c50574e241e40b6dc2
Leonardo Noll Cruz <leonoll8@hotmail.com>
2023-11-22 00:19:47 +0000

    Implementar uma interface grafica em app.java

---
+++ NotasAlunos/src/main/app.java
@@ -0,0 +1,10 @@
+package main;
+
+public class app  {
+    public static void main(String[] args) {
+        System.out.println("========= Notas Alunos =========");
+        System.out.println("* Selecione uma funcionalidade *" );
+        System.out.println("*                              *");
+        System.out.println("*                              *");
+    }
+}
